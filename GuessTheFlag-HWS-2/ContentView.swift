303cb32de531528f4f4ec14e8f51ee5899315f55
Kevin Mattocks <kevin.t.mattocks@gmail.com>
2022-09-16 17:08:12 -0400

    Initial Commit

---
+++ GuessTheFlag-HWS-2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GuessTheFlag-HWS-2
+//
+//  Created by Kevin Mattocks on 9/16/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GuessTheFlag-HWS-2/GuessTheFlag_HWS_2App.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlag_HWS_2App.swift
+//  GuessTheFlag-HWS-2
+//
+//  Created by Kevin Mattocks on 9/16/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlag_HWS_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
