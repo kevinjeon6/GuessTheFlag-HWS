3d52d6496acbbbd7b4269b9ffef2077d6417b099
Kevin Mattocks <kevin.t.mattocks@gmail.com>
2022-09-18 19:31:43 -0400

    Add in countries property which is an array of the different countries Add in correctAnswer property to be in line with the three flags being displayed Add Text and tappable flag buttons for UI

--- GuessTheFlag-HWS-2/ContentView.swift
+++ GuessTheFlag-HWS-2/ContentView.swift
@@ -8,9 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+    
+    var correctAnswer = Int.random(in: 0...2)
+    //There will be 3 flags displayed, 0, 1, 2
+    
     var body: some View {
-        Text("Starting Project 2 of 100 days of SWiftUI")
-            .padding()
+        
+        ZStack {
+            Color.blue
+                .ignoresSafeArea()
+            VStack (spacing: 30) {
+                VStack {
+                    Text("Tap the flag of")
+                        .foregroundColor(.white)
+                    Text(countries[correctAnswer])
+                        .foregroundColor(.white)
+                }
+                
+                ForEach(0..<3) { number in
+                    Button {
+                        //flag was tapped
+                    } label: {
+                        Image(countries[number])
+                            .renderingMode(.original)
+                    }
+                }
+            }
+        }
+        
+   
     }
 }
 
@@ -19,3 +47,13 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+//Zstack
+//If you want to fill out the whole area behind the text. Use Color.red as first thing in Zstack rather than a background modifier
+
+//Color.primary is s adaptive. Depends on user Dark/Light mode
+
+
+
+//background modifer .ultraThinMaterial is very translucent looking
