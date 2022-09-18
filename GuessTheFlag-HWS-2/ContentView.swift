421eb3d534b5043ee46fea483bf66b6e0fee8fc2
Kevin Mattocks <kevin.t.mattocks@gmail.com>
2022-09-18 19:43:48 -0400

    Add state property wrappers to countries and correctAnswer properties Add state properties for showingScore and scoreTitle for the alert message Create method to make sure that the number matches up with the correctAnswer and provide a message if it is correct or incorrect Create a method to move onto next question of guess the flag

--- GuessTheFlag-HWS-2/ContentView.swift
+++ GuessTheFlag-HWS-2/ContentView.swift
@@ -9,9 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     
-    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+    //MARK: - Properties
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
     
-    var correctAnswer = Int.random(in: 0...2)
+    
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    //shuffled randomizes the array for us
+    
+    @State private var correctAnswer = Int.random(in: 0...2)
     //There will be 3 flags displayed, 0, 1, 2
     
     var body: some View {
@@ -29,7 +35,7 @@ struct ContentView: View {
                 
                 ForEach(0..<3) { number in
                     Button {
-                        //flag was tapped
+                    flagTapped(number) //number is being passed into the function to check and see if it matches up with the corect answer
                     } label: {
                         Image(countries[number])
                             .renderingMode(.original)
@@ -37,8 +43,34 @@ struct ContentView: View {
                 }
             }
         }
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue") {
+                askQuestion()
+            }
+        } message: {
+            Text("Your score is ???")
+        }
         
    
+    }
+    
+    
+    //MARK: - Methods
+    
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+        } else {
+            scoreTitle = "Wrong"
+        }
+        
+        showingScore = true
+    }
+    
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
     }
 }
 
